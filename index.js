b351039de00458367bee21b991b05ee0d175be92
ks603 <kevin.shaughnessy18@gmail.com>
2020-06-18 15:13:26 -0400

    Changed settings and practice

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const slugify = require('slugify')
 
 const replaceTemplate = require('./modules/replaceTemplate')
 
-
 // ////////////////////////////////////////////////////////////////
 // FILES
 
@@ -37,16 +36,23 @@ const replaceTemplate = require('./modules/replaceTemplate')
 ////////////////////////////////////
 // SERVER
 
-
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
-
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+)
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+)
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+)
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
-const slugs = dataObj.map(el => slugify(el.productName, { lower: true }))
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }))
 console.log(slugs)
 
 const server = http.createServer((req, res) => {
@@ -58,10 +64,11 @@ const server = http.createServer((req, res) => {
   if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' })
 
-    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('')
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
     res.end(output)
-
 
     //  Product page
   } else if (pathname === '/product') {
@@ -74,14 +81,11 @@ const server = http.createServer((req, res) => {
   } else if (pathname === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' })
     res.end(data)
-
-
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
       // passes meta data with personal headers
-      'my-own-header': 'hello world'
-
+      'my-own-header': 'hello world',
     })
     res.end('<h1>this page not found</h1>')
   }
